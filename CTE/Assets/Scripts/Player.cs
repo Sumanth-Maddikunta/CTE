0758a9fec853f9baaa3a5b501a03ca66f8f73426
Sumanth-Maddikunta <43546822+Sumanth-Maddikunta@users.noreply.github.com>
2019-11-16 11:52:22 +0530

    Basic setup

---
+++ CTE/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed);
+    }
+}
